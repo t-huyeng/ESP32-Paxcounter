8bf88501fdffb2cc4d3bca8f67a95d9a154369c2
Tim Huyeng <huyeng@iib.tu-darmstadt.de>
2022-10-18 16:37:31 +0200

    working hc-sr04 simple example

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -8,9 +8,16 @@ static const char TAG[] = __FILE__;
 #define SENSORBUFFER                                                           \
   10 // max. size of user sensor data buffer in bytes [default=20]
 
+#if (HAS_SENSOR_2)
+uint16_t duration;
+uint16_t distance;
+#endif
 void sensor_init(void) {
   // this function is called during device startup
   // put your user sensor initialization routines here
+  ESP_LOGI(TAG, "sensor_init() called");
+  pinMode(TRIGPIN, OUTPUT); // Sets the trigPin as an Output
+  pinMode(ECHOPIN, INPUT);  // Sets the echoPin as an Input
 }
 
 uint8_t sensor_mask(uint8_t sensor_no) {
@@ -49,11 +56,33 @@ uint8_t *sensor_read(uint8_t sensor) {
     buf[3] = 0x03;
     break;
   case 2:
-    buf[0] = length;
-    buf[1] = 0x01;
-    buf[2] = 0x02;
-    buf[3] = 0x03;
+    ESP_LOGI(TAG, "Inside Sensor 2");
+    digitalWrite(TRIGPIN, LOW);
+    delayMicroseconds(2);
+
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(TRIGPIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGPIN, LOW);
+
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(ECHOPIN, HIGH);
+    ESP_LOGI(TAG, "Measure duration: %d", duration);
+
+    // Calculating the distance
+    distance = duration * 0.034 / 2;
+    ESP_LOGI(TAG, "Measure distance: %d", distance);
+    // use old payload add functions
+    payload.addVoltage(duration);
+    payload.addVoltage(distance);
     break;
+
+    // buf[0] = length;
+    // buf[1] = 0x01;
+    // buf[2] = 0x02;
+    // buf[3] = 0x03;
+    // break;
+
   case 3:
     buf[0] = length;
     buf[1] = 0x01;
