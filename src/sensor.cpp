41d71118081e45b2495d11436878dfbce073fd96
Tim Huyeng <huyeng@iib.tu-darmstadt.de>
2022-11-04 10:27:37 +0100

    working gy21 example with HTU2xD_SHT2x_Si70xx.h library,
    curently sending to port 12 as Voltage

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -2,7 +2,15 @@
 #include "globals.h"
 #include "sensor.h"
 
-#include "gy21read.h"
+#if (HAS_GY21)
+#include <HTU2xD_SHT2x_Si70xx.h>
+
+float htValue; // to store T/RH result
+byte error;
+HTU2xD_SHT2x_SI70xx ht2x(HTU2xD_SENSOR,
+                         HUMD_12BIT_TEMP_14BIT); // sensor type, resolution
+
+#endif // HAS_GY21
 
 // Local logging tag
 static const char TAG[] = __FILE__;
@@ -13,7 +21,13 @@ static const char TAG[] = __FILE__;
 void sensor_init(void) {
   // this function is called during device startup
   // put your user sensor initialization routines here
-  gy21_init(GY21_ADDRESS);
+  if (ht2x.begin() !=
+      true) // reset sensor, set heater off, set resolution, check power
+            // (sensor doesn't operate correctly if VDD < +2.25v)
+  {
+    ESP_LOGE(TAG, "HTU2xD/SHT2x not connected, fail or VDD < +2.25v");
+  }
+  ESP_LOGE(TAG, "HTU2xD/SHT2x/GY21 found");
 }
 
 uint8_t sensor_mask(uint8_t sensor_no) {
@@ -43,14 +57,8 @@ uint8_t *sensor_read(uint8_t sensor) {
   static uint8_t buf[SENSORBUFFER] = {0};
   uint8_t length = 3;
   double temperature, humidity;
-
   switch (sensor) {
   case 1:
-    temperature = gy21_read_temperature(GY21_ADDRESS);
-    humidity = gy21_read_humidity(GY21_ADDRESS);
-    ESP_LOGD(TAG, "GY21: Temperature: %f", temperature);
-    ESP_LOGD(TAG, "GY21: Humidity: %f", humidity);
-    // TODO send data
     // insert user specific sensor data frames here
     buf[0] = length;
     buf[1] = 0x01;
@@ -58,16 +66,23 @@ uint8_t *sensor_read(uint8_t sensor) {
     buf[3] = 0x03;
     break;
   case 2:
+
     buf[0] = length;
     buf[1] = 0x01;
     buf[2] = 0x02;
     buf[3] = 0x03;
     break;
   case 3:
-    buf[0] = length;
-    buf[1] = 0x01;
-    buf[2] = 0x02;
-    buf[3] = 0x03;
+    ESP_LOGE(TAG, "Reading Sensor 3, GY21");
+    temperature =
+        ht2x.readTemperature(); // accuracy +-0.3C in range 0C..60C at  14-bit
+    delay(100);
+    humidity =
+        ht2x.readHumidity(); // accuracy +-2% in range 20%..80%/25C at 12-bit
+    ESP_LOGE(TAG, "GY21: Temperature: %f", temperature);
+    ESP_LOGE(TAG, "GY21: Humidity: %f", humidity);
+    payload.addVoltage(temperature * 100);
+    payload.addVoltage(humidity * 100);
     break;
   }
 
